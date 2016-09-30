c661e9700df6bd954eb5ad7317c8ee31a758cf6a
Kolovaitis <kna.209@yandex.ru>
2016-09-30 18:13:46 +0300

    Initial commit

---
+++ app/src/main/java/by/superteam/supercheckers/CommonDisplayActivity.java
@@ -0,0 +1,13 @@
+package by.superteam.supercheckers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommonDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_display);
+    }
+}
